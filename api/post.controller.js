2b993afe4c9197c5ce85e4cc48797c1d5bbaa402
unknown <sandunpmapa@gmail.com>
2021-03-01 13:14:24 +0530

    Updated router, controller

--- api/post.controller.js
+++ api/post.controller.js
@@ -1,12 +1,23 @@
 const Posts = require("../models/posts");
 
 module.exports = {
+  createPost: async (req, res) => {
+    const newPost = new Posts(req.body);
+    await newPost
+      .save()
+      .then((result) => {
+        res.json({ success: 1, result: result });
+      })
+      .catch((err) => {
+        res.json({ success: 0, result: err });
+      });
+  },
   getPosts: async (req, res) => {
     await Posts.find()
       .then((result) => {
-        if (!result) res.json({ success: false, result: "No results found" });
-        res.json({ success: true, result: result });
+        if (!result) res.json({ success: 0, result: "No results found" });
+        res.json({ success: 1, result: result });
       })
-      .catch((err) => res.json({ success: false, result: err }));
+      .catch((err) => res.json({ success: 0, result: err }));
   },
 };
--- api/post.router.js
+++ api/post.router.js
@@ -1,6 +1,7 @@
-const { getPosts } = require("./post.controller");
+const { getPosts, createPost } = require("./post.controller");
 const router = require("express").Router();
 
 router.get("/", getPosts);
+router.post("/", createPost);
 
 module.exports = router;
