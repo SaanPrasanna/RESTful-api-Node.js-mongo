91bebf57bab0c3ed90a88c2ccbc31e45cd809d1f
unknown <sandunpmapa@gmail.com>
2021-03-01 13:58:05 +0530

    Updated postDelete

--- api/post.controller.js
+++ api/post.controller.js
@@ -12,16 +12,38 @@ module.exports = {
         res.json({ success: 0, result: err });
       });
   },
+
   getPosts: async (req, res) => {
     await Posts.find()
       .then((result) => {
-        if (!result) res.json({ success: 0, result: "No results found" });
+        if (!result) res.json({ success: 0, result: "No posts found" });
         res.json({ success: 1, result: result });
       })
       .catch((err) => res.json({ success: 0, result: err }));
   },
+
+  getPostById: async (req, res) => {
+    await Posts.findById(req.params.id)
+      .then((result) => {
+        if (!result) res.json({ success: 0, result: "No post found" });
+        res.json({ success: 1, result: result });
+      })
+      .catch((err) => res.json({ success: 0, result: err }));
+  },
+
   updatePost: async (req, res) => {
     await Posts.findByIdAndUpdate(req.params.id, req.body)
+      .then((post) => {
+        if (!post) res.json({ success: 0, result: "Post not found!" });
+        res.json({ success: 1 });
+      })
+      .catch((err) => {
+        res.json({ success: 0, result: err });
+      });
+  },
+
+  deletePost: async (req, res) => {
+    await Posts.findByIdAndDelete(req.params.id)
       .then((post) => {
         if (!post) res.json({ success: 0, result: "Post not found!" });
         res.json({ success: 1 });
--- api/post.router.js
+++ api/post.router.js
@@ -1,8 +1,16 @@
-const { getPosts, createPost, updatePost } = require("./post.controller");
+const {
+  getPosts,
+  createPost,
+  updatePost,
+  getPostById,
+  deletePost,
+} = require("./post.controller");
 const router = require("express").Router();
 
 router.get("/", getPosts);
 router.post("/", createPost);
 router.patch("/:id", updatePost);
+router.get("/:id", getPostById);
+router.delete("/:id", deletePost);
 
 module.exports = router;
